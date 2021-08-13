e0496bf583578962dfcd0ab9c386a60033c9e350
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-12 23:14:23 -0500

    secondary changes

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,24 +2,24 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const projectData = require('./currentBlogData.json');
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+const seedDatabase = async() => {
+    await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
     });
-  }
 
-  process.exit(0);
+    for (const project of projectData) {
+        await Project.create({
+            ...project,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
 };
 
 seedDatabase();
