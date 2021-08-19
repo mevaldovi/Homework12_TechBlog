b59b4a774be2c2b1ea913d90c2e168db08422ebf
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-18 23:30:45 -0500

    seeds are done.

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { Blog, Comment, User } = require('../models');
+const blogData = require('./blog-seeds.json');
+const commentData = require('./comment-seeds.json');
+const userData = require('./user-seeds.json');
+
+const seedDatabase = async() => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Blog.bulkCreate(blogData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
